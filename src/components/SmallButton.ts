b3d0aa59ae0d7e0facdd145c3f6016c997106b0c
leofavre <leo@leofavre.com>
2026-02-08 02:52:01 -0300

    buttons with perspective

--- src/components/SmallButton.ts
+++ src/components/SmallButton.ts
@@ -17,10 +17,16 @@ export const SmallButton = styled.button`
   font: inherit;
   font-size: calc(14 / 16 * 1rem);
   white-space: nowrap;
+  box-shadow:
+    1px 1px 0 0 rgb(from var(--color-neutral) r g b / 100%),
+    2px 2px 0 0 rgb(from var(--color-neutral) r g b / 100%),
+    3px 3px 0 0 rgb(from var(--color-neutral) r g b / 100%);
   transition:
-    background-color 0.15s ease,
-    border-color 0.15s ease,
-    color 0.15s ease;
+    background-color 30ms ease-in-out,
+    border-color 30ms ease-in-out,
+    color 30ms ease-in-out,
+    box-shadow 30ms ease-in-out,
+    transform 30ms ease-in-out;
 
   &:hover {
     background-color: rgb(from var(--color-neutral) r g b / 10%);
@@ -38,6 +44,8 @@ export const SmallButton = styled.button`
     background-color: rgb(from var(--color-loud) r g b / 10%);
     border-color: rgb(from var(--color-loud) r g b);
     color: rgb(from var(--color-loud) r g b);
+    box-shadow: none;
+    transform: translate(3px, 3px);
 
     &:hover {
       background-color: rgb(from var(--color-loud) r g b / 20%);
