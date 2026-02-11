f5237bb5e31e740630c66b813c748733e9669f61
leofavre <leo@leofavre.com>
2026-02-11 08:28:18 -0300

    implement ResultBasedDatabaseService

--- src/services/types.ts
+++ src/services/types.ts
@@ -1,3 +1,5 @@
+import type { Result } from "../helpers/errorHandling";
+
 export type DatabaseKey = string;
 
 export type DatabaseService<T, K extends DatabaseKey = string> = {
@@ -6,4 +8,16 @@ export type DatabaseService<T, K extends DatabaseKey = string> = {
   getAllRecords: () => Promise<T[]>;
   updateRecord: (value: T, key?: K) => Promise<void>;
   deleteRecord: (key: K) => Promise<void>;
+};
+
+export type ResultBasedDatabaseService<
+  T,
+  K extends DatabaseKey = string,
+  R extends string = string,
+> = {
+  addRecord: (value: T, key?: K) => Promise<Result<void, R>>;
+  getRecord: (id: number | string) => Promise<Result<T | undefined, R>>;
+  getAllRecords: () => Promise<Result<T[], R>>;
+  updateRecord: (value: T, key?: K) => Promise<Result<void, R>>;
+  deleteRecord: (key: K) => Promise<Result<void, R>>;
 };
