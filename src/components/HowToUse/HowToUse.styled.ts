dff88807b5ade844456804493b17c54d453ba2b7
leofavre <leo@leofavre.com>
2026-03-02 00:47:58 -0300

    hide h2 but not from screen readers

--- src/components/HowToUse/HowToUse.styled.ts
+++ src/components/HowToUse/HowToUse.styled.ts
@@ -32,7 +32,15 @@ export const Content = styled.div`
   }
 
   h2 {
-    display: none;
+    position: absolute !important;
+    width: 1px !important;
+    height: 1px !important;
+    padding: 0 !important;
+    margin: -1px !important;
+    overflow: hidden !important;
+    clip: rect(0, 0, 0, 0) !important;
+    white-space: nowrap !important;
+    border: 0 !important;
   }
 
   ol {
