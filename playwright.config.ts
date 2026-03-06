a521fad386a844542a3a30320007e70fed002489
leofavre <leo@leofavre.com>
2026-03-06 00:41:07 -0300

    handle flaky tests :-/

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,12 +8,14 @@ export default defineConfig({
   testDir: "./e2e",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  retries: process.env.CI ? 2 : 1,
+  workers: process.env.CI ? 1 : 5,
   reporter: "html",
   use: {
     baseURL: "http://localhost:5173",
     trace: "on-first-retry",
+    actionTimeout: 3_000,
+    navigationTimeout: 3_000,
   },
   projects: [{ name: "chromium", use: { ...devices["Desktop Chrome"] } }],
   webServer: {
