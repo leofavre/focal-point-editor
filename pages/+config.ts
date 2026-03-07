3adef9783f5d8d4588f98756d5ce95a94d1ad88e
leofavre <leo@leofavre.com>
2026-03-07 13:08:33 -0300

    test Vercel deploy

--- pages/+config.ts
+++ pages/+config.ts
@@ -1,7 +1,8 @@
 import type { Config } from "vike/types";
+import vikePhoton from "vike-photon/config";
 import vikeReact from "vike-react/config";
 
 export default {
-  extends: [vikeReact],
+  extends: [vikeReact, vikePhoton],
   title: "Focal Point Editor",
 } satisfies Config;
